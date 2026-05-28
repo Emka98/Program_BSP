1f49163f630a585933bee2cecd91c342a46b2f6a
Emka98 <emil.gulinski01@gmail.com>
2026-05-28 16:29:54 +0200

    dodano h

--- emstymacja_kosztów/kosztorys.py
+++ emstymacja_kosztów/kosztorys.py
@@ -1,9 +1,16 @@
+import math
+
 mass = 625
-We = mass * 2.20462262
+M_max = We = mass * 2.20462262
 np = 500
 v_max_ms = 62
 v_max = v_max_ms * 1.944
 T_max = 145_000
+Tem_Prz_Tur = 1200
+Rp = 2
+
+sum_1970 = []
+sum_1986 = []
 
 text_1986 = ""
 text_1970 = ""
@@ -19,12 +26,12 @@ c2_1986= 0.777
 c3_1986 = 0.894
 c4_1986 = 0.163
 
-He_1986 = c1_1986 * We**c2_1986 * v_max**c3_1986 * np**c4_1986
-He_1970 = c1_1970 * We**c2_1970 * v_max**c3_1970 * np**c4_1970
+He_1986 = math.ceil(c1_1986 * We**c2_1986 * v_max**c3_1986 * np**c4_1986)
+He_1970 = math.ceil(c1_1970 * We**c2_1970 * v_max**c3_1970 * np**c4_1970)
 
 wspolny_text = "Czas na rozwój konstrukcji: "
-text_1986 += wspolny_text + str(He_1986) + "h" + "\n"
-text_1970 += wspolny_text + str(He_1970) + "h" + "\n"
+text_1986 += wspolny_text + str(He_1986) + " h" + "\n"
+text_1970 += wspolny_text + str(He_1970) + " h" + "\n"
 
 # Wsparcie prac badawczo rozwojowych [USD]
 c1_1970 = 0.00549
@@ -37,12 +44,15 @@ c2_1986 = 0.63
 c3_1986 = 1.3
 c4_1986 = 0
 
-Cd_1986 = c1_1986 * We **c2_1986 * v_max**c3_1986 * np**c4_1986
-Cd_1970 = c1_1970 * We **c2_1970 * v_max**c3_1970 * np**c4_1970
+Cd_1986 = math.ceil(c1_1986 * We**c2_1986 * v_max**c3_1986 * np**c4_1986)
+Cd_1970 = math.ceil(c1_1970 * We**c2_1970 * v_max**c3_1970 * np**c4_1970)
+
+sum_1986.append(Cd_1986)
+sum_1970.append(Cd_1970)
 
 wspolny_text = "Wsparcie prac badawczo rozwojowych: "
-text_1970 += wspolny_text + str(Cd_1970) + "USD" + "\n"
-text_1986 += wspolny_text + str(Cd_1986) + "USD" + "\n"
+text_1970 += wspolny_text + str(Cd_1970) + " USD" + "\n"
+text_1986 += wspolny_text + str(Cd_1986) + " USD" + "\n"
 
 # Cena silnika i awioniki [USD]
 c1_1970 = 130
@@ -54,13 +64,125 @@ c3_1986 = 3.25
 c4_1986 = 0.969
 c5_1986 = 2228.0
 
-Cen_1970 = c1_1970 * T_max**c2_1970
-Cen_1986 = c1_1986 * (c2_1986 * T_max)
+Cen_1970 = math.ceil(c1_1970 * T_max**c2_1970)
+Cen_1986 = math.ceil(c1_1986 * (c2_1986 * T_max + c3_1986 * M_max + c4_1986 * Tem_Prz_Tur - c5_1986))
 
+sum_1986.append(Cen_1986)
+sum_1970.append(Cen_1970)
 
-# Robocizna 
-Cen_1986 = c1_1986 * We **c1_1986 * v_max**c3_1986 * np**c4_1986
-print("Rok 1970:")
-print(text_1970)
-print("Rok 1986:")
-print(text_1986)
+wspolny_text = "Cena silnika i awioniki: "
+text_1970 += wspolny_text + str(Cd_1970) + " USD" + "\n"
+text_1986 += wspolny_text + str(Cd_1986) + " USD" + "\n"
+
+# Robocizna [h]
+c1_1970 = 20.348
+c2_1970 = 0.740
+c3_1970 = 0.543
+c4_1970 = 0.524
+
+c1_1986 = 7.370
+c2_1986 = 0.820
+c3_1986 = 0.484
+c4_1986 = 0.641
+
+Hml_1970 = c1_1970 * We**c2_1970 * v_max**c3_1970 * np**c4_1970
+Hml_1986 = c1_1986 * We**c2_1986 * v_max**c3_1986 * np**c4_1986
+
+wspolny_text = "Robocizna: "
+text_1970 += wspolny_text + str(Hml_1970) + " h" + "\n"
+text_1986 += wspolny_text + str(Hml_1986) + " h" + "\n"
+
+# Materiały [USD]
+c1_1970 = 18.47
+c2_1970 = 0.689
+c3_1970 = 0.624
+c4_1970 = 0.792
+
+c1_1986 = 11.00
+c2_1986 = 0.921
+c3_1986 = 0.621
+c4_1986 = 0.799
+
+Cmm_1970 = c1_1970 * We**c2_1970 * v_max**c3_1970 * np**c4_1970
+Cmm_1986 = c1_1986 * We**c2_1986 * v_max**c3_1986 * np**c4_1986
+
+sum_1986.append(Cmm_1986)
+sum_1970.append(Cmm_1970)
+
+wspolny_text = "Materiały: "
+text_1970 += wspolny_text + str(Cmm_1970) + " USD" + "\n"
+text_1986 += wspolny_text + str(Cmm_1986) + " USD" + "\n"
+
+# Oprzyrządowanie i narzędzia [h]
+c1_1970 = 2.79
+c2_1970 = 0.764
+c3_1970 = 0.899
+c4_1970 = 0.178
+c5_1970 = 0.066
+
+c1_1986 = 5.99
+c2_1986 = 0.777
+c3_1986 = 0.696
+c4_1986 = 0.263
+c5_1986 = 0
+
+Ht_1970 = c1_1970 * We**c2_1970 * v_max**c3_1970 * np**c4_1970 * Rp**c5_1970
+Ht_1986 = c1_1986 * We**c2_1986 * v_max**c3_1986 * np**c4_1986
+
+wspolny_text = "Oprzyrządowanie i narzędzia: "
+text_1970 += wspolny_text + str(Hml_1970) + " h" + "\n"
+text_1986 += wspolny_text + str(Hml_1986) + " h" + "\n"
+
+# System zapewnienia jakości [h]
+c1_1986 = c1_1970 = 0.13
+
+Hqc_1970 = c1_1970 * Hml_1970
+Hqc_1986 = c1_1986 * Hml_1986
+
+wspolny_text = "System zapewnienia jakości: "
+text_1970 += wspolny_text + str(Hqc_1970) + " h" + "\n"
+text_1986 += wspolny_text + str(Hqc_1986) + " h" + "\n"
+
+# Próby w locie (zaniedbywane w fazie produkcji) [USD]
+c1_1970 = 0.000714
+c2_1970 = 1.160
+c3_1970 = 1.371
+c4_1970 = 1.281
+
+c1_1986 = 243.03
+c2_1986 = 0.325
+c3_1986 = 0.822
+c4_1986 = 1.210
+
+Cft_1970 = c1_1970 * We**c2_1970 * v_max**c3_1970 * np**c4_1970
+Cft_1986 = c1_1986 * We**c2_1986 * v_max**c3_1986 * np**c4_1986
+
+sum_1986.append(Cft_1986)
+sum_1970.append(Cft_1970)
+
+wspolny_text = "Próby w locie: "
+text_1970 += wspolny_text + str(Cft_1970) + " USD" + "\n"
+text_1986 += wspolny_text + str(Cft_1986) + " USD" + "\n"
+
+# Stawki godzinowe [USD]
+# c1_1970 = 16.0
+# c2_1970 = 11.50
+# c3_1970 = 10.0
+# c4_1970 = 10.0
+
+# c1_1986 = 59.10
+# c2_1986 = 61.70
+# c3_1986 = 55.40
+# c4_1986 = 50.10
+
+# Cp_1970 = c1_1970 * We**c2_1970 * v_max**c3_1970 * np**c4_1970
+# Cp_1986 = c1_1986 * We**c2_1986 * v_max**c3_1986 * np**c4_1986
+
+# wspolny_text = "Stawki godzinowe: "
+# text_1970 += wspolny_text + str(Cft_1970) + " h" + "\n"
+# text_1986 += wspolny_text + str(Cft_1986) + " h" + "\n"
+
+print(f"Rok 1970: \n{text_1970}")
+print(f"Rok 1986:\n{text_1986}")
+
+print(sum(sum_1970))
